f6b0c78dc0f4636f0e6a0b85338c8fb27a198f5c
sarxell <sarapombinho@ua.pt>
2022-04-20 21:45:48 +0100

    #30 rpm speed created to compare

--- atlascar2_description/nodes/CANmsgs_to_ackermann.py
+++ atlascar2_description/nodes/CANmsgs_to_ackermann.py
@@ -11,12 +11,9 @@ from ackermann_msgs.msg import AckermannDriveStamped
 import math
 
 ack_pub = rospy.Publisher("atlascar2/ackermann_msgs", AckermannDriveStamped, queue_size=10)
-current_time = rospy.Time.now()
-last_time = rospy.Time.now()
-
+# rate not working, need to see why
 
 def receive_all():
-    global last_time, current_time
     """Receives all messages and prints them to the console until Ctrl+C is pressed."""
     steering_angle = None
     speed = None
@@ -34,32 +31,33 @@ def receive_all():
                 if msg.arbitration_id == 0x412:
                     # speed is in km/h
                     speed_km = msg.data[1]
-                    dt = (current_time - last_time).to_sec()
-                    speed_ms = (speed_km*1000)/dt
-                    # ackMsg.drive.speed = speed
-                    last_time = current_time
-                if msg.arbitration_id == 0x298:
-                    motor_rpm = msg.data[6]*256 - 1000
-                    speed = (motor_rpm*math.pi*0.285)/30
+                    print(msg.data[1])
+                    speed = (speed_km * 1000)/3600
                     ackMsg.drive.speed = speed
                     print(speed)
+
+                if msg.arbitration_id == 0x298:
+                    motor_rpm = (msg.data[6]*256 + msg.data[7]) - 10000
+                    speed_rpm = (motor_rpm*math.pi*0.285)/(30*7.065)
+                    # ackMsg.drive.speed = speed
+                    print(speed_rpm)
+
                 if msg.arbitration_id == 0x236:
                     # to get the steering angle its the following formula:
                     # ((B0*256 + B1) -4096)/2
                     # sendo B1 o byte 1 do identificador 0x412 e B0 0 byte 0 do identificador 0x236
                     steering_angle = ((msg.data[0] * 256 + msg.data[1]) - 4096) / 2
-                    ackMsg.drive.steering_angle = steering_angle
+                    # steering needs to be in radians
+                    ackMsg.drive.steering_angle = (math.pi*steering_angle)/180
 
                 if (steering_angle is not None) & (speed is not None):
                     ackMsg.header.stamp = rospy.Time.now()
                     ackMsg.header.frame_id = "atlascar2/ackermann_msgs"
                     ack_pub.publish(ackMsg)
-                    print(steering_angle)
-                    rate.sleep()
+            rate.sleep()
 
 
 def main():
-
     rospy.init_node('ackermann_publisher')
     receive_all()
     rospy.spin()
