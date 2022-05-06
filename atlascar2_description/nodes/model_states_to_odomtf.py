c427546cd6c87d862e39c8f4e07496a9233bd4bd
manuelgitgomes <gomesnelito@gmail.com>
2022-05-06 17:00:10 +0100

    #32 Fully functional calibration

--- atlascar2_description/nodes/model_states_to_odomtf.py
+++ atlascar2_description/nodes/model_states_to_odomtf.py
@@ -8,27 +8,25 @@ from geometry_msgs.msg import TransformStamped
 
 def modelStatesCallback(message, states):
 
+    # If the model is atlascar2, get its model state and send the tf
     for idx, model in enumerate(message.name):
         if model == "atlascar2":
             states['pose'] =  message.pose[idx]
-            states['transform'].header.stamp = rospy.Time.now()
-            states['transform'].transform.translation = states['pose'].position
-            states['transform'].transform.rotation = states['pose'].orientation
-            states['broadcaster'].sendTransform(states['transform'])
+            states['start'] = True
 
 
 def main():
 
     rospy.init_node('model_states_to_tf')
-    
+
+    # t is the transform to send and br is the broadcaster to send it    
     br = tf2_ros.TransformBroadcaster()
     t = TransformStamped()
     t.header.stamp = rospy.Time.now()
     t.header.frame_id = 'odom'
     t.child_frame_id = 'base_footprint'
 
-
-    states = {'pose': [], 'transform': t, 'broadcaster': br}
+    states = {'pose': [], 'transform': t, 'broadcaster': br, 'start': False}
 
     modelStatesPartialCallback = partial(modelStatesCallback, states=states)
     
@@ -37,8 +35,11 @@ def main():
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        print(states)
-
+        if states['start']:
+            states['transform'].header.stamp = rospy.Time.now()
+            states['transform'].transform.translation = states['pose'].position
+            states['transform'].transform.rotation = states['pose'].orientation
+            states['broadcaster'].sendTransform(states['transform'])
         rate.sleep()
 
 
