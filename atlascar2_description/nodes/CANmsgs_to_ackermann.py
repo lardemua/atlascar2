812e4f47b1eeaa923b26c0b4974c1204e12df423
sarxell <sarapombinho@ua.pt>
2022-05-25 13:58:05 +0100

    can msgs changed

--- atlascar2_description/nodes/CANmsgs_to_ackermann.py
+++ atlascar2_description/nodes/CANmsgs_to_ackermann.py
@@ -16,8 +16,8 @@ ack_pub = rospy.Publisher("atlascar2/ackermann_msgs", AckermannDriveStamped, que
 def receive_all():
     """Receives all messages and prints them to the console until Ctrl+C is pressed."""
     global ack_pub
-    steering_angle = None
-    speed = None
+    steering_angle = 0
+    speed = 0
     ackMsg = AckermannDriveStamped()
     rate = rospy.Rate(100)
 
@@ -25,28 +25,41 @@ def receive_all():
         # right now I need the B1 with the ID 0x412 and B0 with the ID 0x236
         while not rospy.is_shutdown():
             msg = bus.recv(1)
-            print(msg.arbitration_id)
             if msg is not None:
-                if msg.arbitration_id == 0x412:
-                    # speed is in km/h
-                    speed_km = msg.data[1]
-                    speed_ms = (speed_km * 1000)/3600
-                    # print(speed_ms)
+                # if msg.arbitration_id == 0x412:
+                #     # speed is in km/h
+                #     speed_km = msg.data[1]
+                #     speed_ms = (speed_km * 1000)/3600
+                #     # print(speed_ms)
 
-                if msg.arbitration_id == 0x298:
-                    motor_rpm = (msg.data[6]*256 + msg.data[7]) - 10000
-                    # 7.065 -> final drive ratio from the vehicle
-                    # 0.285 -> radius of the wheel
-                    speed = (motor_rpm*math.pi*0.285)/(30*7.065)
-                    # print(speed)
+                if msg.arbitration_id == 0x500:
+                    # speed is in km/h
+                    speed = int.from_bytes(msg.data, "big", signed=True)
                     ackMsg.header.stamp = rospy.Time.now()
                     ackMsg.header.frame_id = "atlascar2/ackermann_msgs"
                     ackMsg.drive.speed = speed
                     # steering needs to be in radians
                     ackMsg.drive.steering_angle = (math.pi * steering_angle) / 180
                     # print(speed, steering_angle)
+                    print(speed, steering_angle)
                     ack_pub.publish(ackMsg)
                     rate.sleep()
+
+
+                # if msg.arbitration_id == 0x298:
+                #     motor_rpm = (msg.data[6]*256 + msg.data[7]) - 10000
+                #     # 7.065 -> final drive ratio from the vehicle
+                #     # 0.285 -> radius of the wheel
+                #     speed = (motor_rpm*math.pi*0.285)/(30*7.065)
+                #     # print(speed)
+                #     ackMsg.header.stamp = rospy.Time.now()
+                #     ackMsg.header.frame_id = "atlascar2/ackermann_msgs"
+                #     ackMsg.drive.speed = speed
+                #     # steering needs to be in radians
+                #     ackMsg.drive.steering_angle = (math.pi * steering_angle) / 180
+                #     # print(speed, steering_angle)
+                #     ack_pub.publish(ackMsg)
+                #     rate.sleep()
 
                 else:
                     if msg.arbitration_id == 0x236:
@@ -62,10 +75,8 @@ def receive_all():
                         ackMsg.drive.steering_angle = (math.pi * steering_angle) / 180
                         # print(speed)
                         ack_pub.publish(ackMsg)
+                        print(speed, steering_angle)
                         rate.sleep()
-
-                if msg.arbitration_id == 0x500:
-                    print(msg.data)
 
 
 def main():
